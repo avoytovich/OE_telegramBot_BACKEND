be12ba2e4876c533c8fdc4a9f08dda23eeb17ecc
avoytovich <levanwork@gmail.com>
2020-05-02 15:51:41 +0300

    add: grud list subgroup

--- server/controllers/subGroupOfBookmark.controller.js
+++ server/controllers/subGroupOfBookmark.controller.js
@@ -1,10 +1,21 @@
 const { SubGroupOfBookmarks } = require('./../models');
 
 module.exports = {
-    create(req, res) {
-        const dataCreate = Object.assign({}, req.body, {GroupOfBookmarksId: req.params.group})
-        SubGroupOfBookmarks.create(dataCreate)
-            .then(groupOfBookmarks => res.status(200).json({message: 'subGroupOfBookmarks was created!'}))
-            .catch(error => res.status(404).send(error));
-    }
+  create(req, res) {
+    const dataCreate = Object.assign({}, req.body, {
+      GroupOfBookmarksId: req.params.group,
+    });
+    SubGroupOfBookmarks.create(dataCreate)
+      .then((groupOfBookmarks) =>
+        res.status(200).json({ message: 'subGroupOfBookmarks was created!' })
+      )
+      .catch((error) => res.status(404).send(error));
+  },
+  list(req, res) {
+    SubGroupOfBookmarks.findAll({
+      where: { GroupOfBookmarksId: req.params.group },
+    })
+      .then((subGroups) => res.status(200).json({ subGroups }))
+      .catch((error) => res.status(404).send(error));
+  },
 };
